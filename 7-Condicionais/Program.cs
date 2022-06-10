6d583f2eea0925c0a9b2080e047515af4f3b3216
Jimmy <edygatojr@live.com>
2022-06-10 00:30:54 -0300

    Criando e Identando Projeto 7 - Condicionais Atalho CTRL + K em seguida CTRL + D

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoa = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade.\nEntrada Liberada!");
+            }
+            else
+            {
+                if (quantidadePessoa >= 2)
+                {
+                    Console.WriteLine("João não possui mais de 18 anos, mas está acompanhando.\nEntrada Liberada!");
+                }
+                else
+                {
+                    Console.WriteLine("João possui menos de 18 anos de idade.\nEntrada Bloqueada!");
+                }
+
+            }
+
+            Console.WriteLine("\nExcecução Finalizada! Clique enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

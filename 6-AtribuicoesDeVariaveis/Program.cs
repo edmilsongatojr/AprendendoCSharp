6219e5771e51ea139521384d7baf09c36ebbec16
Jimmy <edygatojr@live.com>
2022-06-10 00:21:11 -0300

    Criando Projeto 6 - Atribuicoes De Variaveis.

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 6 - Atribuicoes De Variaveis");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.WriteLine("\nExcecução Finalizada! Clique enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

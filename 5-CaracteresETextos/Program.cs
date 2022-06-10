13d12fdce1d507c7663aa4f9dddf6e84596aba93
Jimmy <edygatojr@live.com>
2022-06-10 00:15:23 -0300

    Criando Projeto 5 - Caracteres E Textos.

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 5 - Caracteres E Textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = " \n- .NET \n- Java \n- Javascript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("\nExcecução Finalizada! Clique enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

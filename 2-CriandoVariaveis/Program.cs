284963c9defc5bb7d5bc764bdee67b00564355fb
Jimmy <edygatojr@live.com>
2022-06-09 22:33:18 -0300

    Conclusão dos Consoles e Váriaveis com cálculos básicos.

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -15,19 +15,25 @@ namespace _2_CriandoVariaveis
             int idade;
             idade = 32;
 
-            Console.WriteLine(idade);
+            Console.WriteLine("Idade sem operadores: "+idade);
 
             idade = 10;
-            Console.WriteLine(idade);
+            Console.WriteLine("Idade com o dados inserido alterado: "+idade);
 
             idade = 10 + 5;
-            Console.WriteLine(idade);
+            Console.WriteLine("Idade com operador +: " + idade);
 
             idade = 10 + 5 * 2;
-            Console.WriteLine(idade);
+            Console.WriteLine("Idade com operador *: " + idade);
+
+            idade = 10 + 5 / 2;
+            Console.WriteLine("Idade com operador /: " + idade);
+
+            idade = 10 - 2;
+            Console.WriteLine("Idade com operador -: " + idade);
 
             idade = (10 + 5) * 2;
-            Console.WriteLine("Sua idade é: " + idade+"!");
+            Console.WriteLine("Idade com () para definir prioridade no cálculo" + idade+"!");
 
             Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
             Console.ReadLine();

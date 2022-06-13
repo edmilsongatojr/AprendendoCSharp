992d0a8e4df0cd1ba02cc0857a9f29b62a1d0e19
Jimmy <edygatojr@live.com>
2022-06-13 20:17:25 -0300

    Criando Projeto 10 - Calcudora de Poupança. Estrutura While. variavel++;

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 10 - Calcudora de Poupança");
+
+            double valorInvestido = 1000.00;
+            double jurosPoupanca = 0.0036;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido += valorInvestido *  jurosPoupanca;
+                Console.WriteLine("Após "+ contadorMes +"° mês, você terá R$ " + valorInvestido);
+                contadorMes++;
+            }
+
+            
+            Console.WriteLine("\nExcecução Finalizada! Clique enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
